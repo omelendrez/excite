c1fbf2da43f4947170505803c81882d8d9b829bb
omelendrez <omar.melendrez@gmail.com>
2020-11-18 20:00:55 -0300

    Implement NavLink to home button

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 
 const Notfound = () => {
-  const goHome = e => {
-    e.preventDefault()
-    window.location.href = '/'
-  }
+
   return (
     <div className="container text-center p-5">
       <h1 className="text-danger my-5">OOPS!</h1>
       <p className="lead my-5">La página que está buscando aún no ha sido creada</p>
-      <button className="btn btn-danger" onClick={e => goHome(e)}>
+      <NavLink className="btn btn-danger" to="/">
         <i className="fas fa-home"></i> Volver a la Home Page
-      </button>
+      </NavLink>
     </div>
   )
 }
