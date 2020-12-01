51a1595be09c54f21b77636ae2f00d8850e3278c
omelendrez <omar.melendrez@gmail.com>
2020-11-30 21:11:52 -0300

    Remve obs from transportes

--- src/components/transportes/columns.js
+++ src/components/transportes/columns.js
@@ -4,22 +4,26 @@ export const columns = [
     label: "Código",
     numeric: true,
     align: "right",
-    disablePadding: true
+    disablePadding: true,
+    minWidth: 60
   },
   {
     id: "TRANOM",
     label: "Nombre",
     numeric: false,
+    minWidth: 230
   },
   {
     id: "TRADOM",
     label: "Domicilio",
     numeric: false,
+    minWidth: 200
   },
   {
     id: "TRALOC",
     label: "Localidad",
     numeric: false,
+    minWidth: 200
   },
   {
     id: "TRATEL",
@@ -29,11 +33,6 @@ export const columns = [
   {
     id: "TRAOBS",
     label: "Observaciones",
-    numeric: false,
-  },
-  {
-    id: "TRACUIT",
-    label: "CUIT",
     numeric: false,
   }
 ]
